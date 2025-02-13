798b1b54b767384718919140d35ef78b2d4e9617
miguelNPB <miguelnp@ucm.es>
2025-02-13 12:36:43 +0100

    merodear

--- Assets/Scripts/Comportamientos/Merodear.cs
+++ Assets/Scripts/Comportamientos/Merodear.cs
@@ -18,26 +18,41 @@ namespace UCM.IAV.Movimiento
     /// <summary>
     /// Clase para modelar el comportamiento de WANDER a otro agente
     /// </summary>
-    public class Merodear : ComportamientoAgente
+    public class Merodear : Llegada
     {
+        [SerializeField]
+        float radioWander = 5.0f;
+
         [SerializeField]
         float tiempoMaximo = 2.0f;
 
         [SerializeField]
         float tiempoMinimo = 1.0f;
 
-        float t = 3.0f;
-        float actualT = 2.0f;
+        float timer = 0f;
 
         ComportamientoDireccion lastDir = new ComportamientoDireccion();
 
-        public override ComportamientoDireccion GetComportamientoDireccion(){
-            // IMPLEMENTAR merodear
-            tiempoMinimo = 1.0f; // por ejemplo
-            tiempoMaximo = 2.0f; // por ejemplo
-            actualT = 2.0f; // por ejemplo
-            t = 3.0f; // por ejemplo    
-            return new ComportamientoDireccion();
+        public virtual ComportamientoDireccion GetComportamientoDireccion(){
+
+            timer -= Time.deltaTime;
+
+            // si acaba el tiempo de merodear volvemos a generar dir
+            if (timer <= 0) {
+                if (!objetivo)
+                    objetivo = new GameObject();
+
+                float newX = transform.position.x + Mathf.Sin(Random.Range(-1,1));
+                float newZ = transform.position.z + Mathf.Cos(Random.Range(-1,1));
+
+                objetivo.transform.position = new Vector3(newX, transform.position.y, newZ);
+
+                timer = Random.Range(tiempoMinimo, tiempoMaximo);
+            }
+
+            lastDir = base.GetComportamientoDireccion();
+
+            return lastDir;
         }
 
     }
