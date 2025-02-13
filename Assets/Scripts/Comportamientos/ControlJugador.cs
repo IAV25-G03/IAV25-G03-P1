efebdb646a17f61959827948b77908d9a3343adc
ADDI01 <dapala02@ucm.es>
2025-02-13 12:46:26 +0100

    Raycast sin movimiento correcto

--- Assets/Scripts/Comportamientos/ControlJugador.cs
+++ Assets/Scripts/Comportamientos/ControlJugador.cs
@@ -11,7 +11,6 @@
 namespace UCM.IAV.Movimiento
 {
     using UnityEngine;
-
     /// <summary>
     /// El comportamiento de agente que consiste en ser el jugador
     /// </summary>
@@ -21,14 +20,43 @@ namespace UCM.IAV.Movimiento
         /// Obtiene la dirección
         /// </summary>
         /// <returns></returns>
+        /// 
+
+        Camera cam;
+        public LayerMask mask;
+
+        public void Start()
+        {
+            cam = Camera.main;
+        }
 
         public override ComportamientoDireccion GetComportamientoDireccion()
         {
             ComportamientoDireccion direccion = new ComportamientoDireccion();
             
-            //Direccion actual
+            //Direccion actual (se gestiona a traves de la configuracion de Unity) -> Cambiar por Raycast
             direccion.lineal.x = Input.GetAxis("Horizontal");
             direccion.lineal.z = Input.GetAxis("Vertical");
+            
+            //TO-DO: ARREGLAR MOVIMIENTO
+            /*//Draw Ray (comprobacion de que funciona el Raycast)
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = cam.ScreenToWorldPoint(mousePos);
+            Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask)) //Colision con el suelo -> Mover
+                {
+                    Debug.Log("Holaa");
+                    Debug.Log(ray.GetPoint(100));
+                    direccion.lineal.x = mousePos.x - transform.position.x;
+                    direccion.lineal.z = mousePos.y - transform.position.y;
+                }
+            }*/
 
             //Resto de cálculo de movimiento
             direccion.lineal.Normalize();
